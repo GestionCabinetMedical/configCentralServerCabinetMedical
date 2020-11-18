71502b685a4691342a7c14e75c3d57759f522e2a
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-11-18 13:55:31 +0100

    initialisation config central

---
+++ src/test/java/com/ConfigCentralServerCabinetMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCentralServerCabinetMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
